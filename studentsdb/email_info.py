4a42389fcac50ee0c559058b5cc17acbde70b02d
Vadim <roxy_10@mail.ru>
2016-06-13 11:32:09 +0300

    Delete email_info.py

--- studentsdb/email_info.py
+++
@@ -1,8 +0,0 @@
-# email settings
-# please, set here you smtp server details and your admin email
-ADMIN_EMAIL = 'djeffik@gmail.com'
-EMAIL_HOST = 'smtp.mail.ru'
-EMAIL_PORT = '2525'
-EMAIL_HOST_USER = 'roxy_10@mail.ru'
-EMAIL_HOST_PASSWORD = '3526068'
-EMAIL_USE_TLS = True
